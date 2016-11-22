9e29a2bf3e63b5cd76e9c83a0fa971efce333488
OmarSER <oroldanm@asu.edu>
2016-11-22 14:37:23 -0700

    New commit

--- SER215_Project/src/hangman_Project/GameBoard.java
+++ SER215_Project/src/hangman_Project/GameBoard.java
@@ -1,58 +1,7 @@
 package hangman_Project;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-
-import project_HangMan.Hangman;
-import project_HangMan.LetterGrid;
 
 public class GameBoard extends JFrame{
-	
-	//Game board width
-	private final int width;
-		
-	//Game board height
-	private final int height;
-		
-	//Max num of guesses
-	private final int maxWrong;
-	
-	//Max word length
-	private final int maxWordLength;
-	
-	//Hangman images dir
-	private final String hangmanImageDir;
-	
-	//Image type
-	private final String hangmanType;
-	
-	//Base name
-	private final String hangmanBaseName;
-	
-	//Letter dir
-	private final String letterImageDirectory;
-	
-	//Letter image type
-	private final String letterType;
-	
-	//Letter grid containing the letters
-	private LetterGrid gameGrid;
-	
-	//Image holder
-	private Hangman gameHangman;
-	
-	private int numWrong;
-	
-	//Display hidden letter guessed
-	private JLabel correct;
-		
-	//Displays number of incorrect tries
-	private JLabel wrong;
-	
-	//Word to be guessed
-	private String selectedWord;
-	
-	//StringBuilder (hide the words until guessed correctly)
-	private StringBuilder wordHidden;
 
 }
